97fae756b5a1db8dd5dc1d0e57c590004b5ceded
stepami <stepami@mail.ru>
2023-06-08 19:17:10 +0300

    v2

--- PolymorphicContracts.AutoFixture/FixtureExtensions.cs
+++ PolymorphicContracts.AutoFixture/FixtureExtensions.cs
@@ -4,6 +4,6 @@ namespace PolymorphicContracts.AutoFixture;
 
 public static class FixtureExtensions
 {
-    public static PolymorphicCustomizationBuilder<TBaseType> CustomizePolymorphism<TBaseType>(this Fixture fixture)
+    public static PolymorphicCustomizationBuilder<TBaseType> CustomizePolymorphismFor<TBaseType>(this Fixture fixture)
         where TBaseType : class => new(fixture);
 }
